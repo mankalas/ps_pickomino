d3bbef4a1846ca1b9249c9d10a5a1cea749ff16c
Vincent Boucheny <vincent.boucheny@powershop.co.nz>
2016-08-30 15:32:01 +1200

    Player create action implemented. Welcome#index displays players.

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,2 +1,17 @@
 class PlayersController < ApplicationController
+  def create
+    @player = Player.new(player_params)
+
+    if @player.save
+      redirect_to welcome_index_path
+    else
+      redirect_to new_players_path, notice: @player.errors
+    end
+  end
+
+  private
+
+  def player_params
+    params.require(:player).permit(:name, :color)
+  end
 end
