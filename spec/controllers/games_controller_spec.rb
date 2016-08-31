b21a20e35d588e5e5b60a0193b5e0cb1c1137415
Vincent Boucheny <vincent.boucheny@powershop.co.nz>
2016-08-31 16:07:10 +1200

    Spec for Game controller.

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe GamesController, type: :controller do
+  describe "GET #show" do
+    let(:game) { Game.new }
 
+    it "renders the 'show' template" do
+      game.save!
+      get :show, params: { id: game.id }
+      expect(response).to render_template(:show)
+    end
+  end
+
+  describe "POST #create" do
+    let(:create_request) { post :create }
+
+    it "creates a new game" do
+      expect{ create_request }.to change{ Game.count }.by(1)
+    end
+
+    it "redirects to the freshly created game" do
+      create_request
+      expect(response).to redirect_to(Game.last)
+    end
+  end
 end
